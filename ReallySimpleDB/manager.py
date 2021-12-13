43383b8f544e1315743dbd4385f922de52d25e66
Tharindu N <tharindu.nm@yahoo.com>
2021-12-13 18:42:59 +0530

    add_columns and create_table function

--- ReallySimpleDB/manager.py
+++ ReallySimpleDB/manager.py
@@ -1,17 +1,63 @@
 import os
 import sqlite3
+from typing import List
+from .utils import DATA_TYPES
 
 class ReallySimpleDB:
     def __init__(self) -> None:
-        pass
+        self._add_columns_cmd = ""
+
+    def __create_connection(self, database):
+        self.connection = sqlite3.connect(database)
 
     def create_db(self, dbpath, replace=False):
-        if replace or not os.path.isfile(os.path.realpath(dbpath)):
-            self.connection = sqlite3.connect(dbpath)
-            return True
+        if replace:
+            if os.path.isfile(os.path.realpath(dbpath)):
+                os.remove(os.path.realpath(dbpath))
 
+        if not os.path.isfile(os.path.realpath(dbpath)):
+            self.connection = sqlite3.connect(os.path.realpath(dbpath))
+            return True
         else:
             raise FileExistsError("'{}' file exists. for replace add parameter 'replace=True'".format(dbpath))
+
+    def add_columns(self, column_name:str, datatype:str="TEXT", primary_key:bool=False, NOT_NULL:bool=False, database:str="", table:str=""):
+        if datatype.upper() not in DATA_TYPES:
+            raise TypeError("datatype not supported, '{}'".format(datatype))
+
+        if database != "":
+            if table == "":
+                raise TypeError ("add_columns() missing 1 required positional argument: 'table'")
+            self.__create_connection(database=database)
+            cursor = self.connection.cursor()
+            sql_cmd = "ALTER TABLE {} ADD COLUMN {} {}".format(table, column_name, datatype)
+            if NOT_NULL:
+                sql_cmd += " NOT NULL"
+            if primary_key:
+                sql_cmd += " PRIMARY KEY"
+            cursor.execute(sql_cmd)
+            return True
+
+        self._add_columns_cmd += (",{} {}".format(column_name, datatype))
+
+        if primary_key:
+            self._add_columns_cmd += " PRIMARY KEY"
+
+        if NOT_NULL:
+            self._add_columns_cmd += " NOT NULL"
+
+    def create_table(self, database:str, table_name:str):
+        if self._add_columns_cmd == "":
+            raise NotImplementedError("call 'add_columns' function before create table")
+        
+        sql_cmd = "CREATE TABLE {} ({})".format(table_name, self._add_columns_cmd[1:])
+
+        self.__create_connection(database)
+        self.connection.execute(sql_cmd)
+        return True
+
+    def delete_table(self, database, table_name):
+        pass
 
     def close_connection(self):
         self.connection.close()
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,12 +1,27 @@
-import pytest
 import os
 from ReallySimpleDB import dbmanager
 
 _dbmanager = dbmanager()
 
 def test_create_db():
-    value = _dbmanager.create_db("test.db")
-    if value:
+    if _dbmanager.create_db("test.db", replace=True):
         _dbmanager.close_connection()
         os.remove("test.db")
-    assert value
+        assert True
+
+def test_create_table():
+    _dbmanager.create_db("test.db", replace=True)
+    _dbmanager.close_connection()
+
+    _dbmanager.add_columns(column_name="student_id", primary_key=True)
+    _dbmanager.add_columns(column_name="name", NOT_NULL=True)
+    _dbmanager.add_columns(column_name="mark", datatype="INT")
+    if _dbmanager.create_table(database="test.db", table_name="STUDENTS"):
+        _dbmanager.close_connection()
+        assert True
+
+def test_add_columns():
+    if _dbmanager.add_columns(column_name="year", database="test.db", table="STUDENTS"):
+        _dbmanager.close_connection()
+        os.remove("test.db")
+        assert True
