fd762bc8a43bd50384c55562db9113e6aeed8fad
Tharindu N <tharindu.nm@yahoo.com>
2022-02-24 10:40:24 +0530

    version 1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="ReallySimpleDB",
-    version="1.0",
+    version="1.1",
     description="A tool for easily manage databases with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
